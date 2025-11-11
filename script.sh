9ea0a3fabe9be579d50f700925f6d2b95778041d
Bassoon Valley <rangabrunda@outlook.com>
2025-11-11 19:42:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 # Define long image names with readable labels
 IMAGE1="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitanetskeyv2customnoipdirectgitactrun0539:006-of-one"
-IMAGE2="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitbnetskeyv2customnoipdirectgitactrun0539:006-of-two"
-IMAGE3="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitcnetskeyv2customnoipdirectgitactrun0539:006-of-three"
+IMAGE2="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitanetskeyv2customnoipdirectgitactrun0539:006-of-one"
+IMAGE3="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitanetskeyv2customnoipdirectgitactrun0539:006-of-one"
 
 # Put them into an array in order
 IMAGES=("$IMAGE1" "$IMAGE2" "$IMAGE3")
